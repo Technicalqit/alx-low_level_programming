b2a9d7480029100a543d89c1d2c285b976b9d62d
Technicalqit <collinsomanudhowo2@gmail.com>
2022-07-04 10:54:46 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a < 10; a++)
+		putchar((a % 10) + '0');
+	for (b = 'a'; b <= 'f'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
