f42f51c2f8461396ab599248e61383cd95f6f139
Your Name <collinsomanudhowo2@gmail.com>
2022-07-05 19:30:43 -0700

    0 _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char put[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(put[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
