8073709842f8978b3ed760df01d6e1d5e8905378
Your Name <collinsomanudhowo2@gmail.com>
2022-07-06 10:17:16 -0700

    I got weakened

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	{
 		for (column = 0; column <= 9; column++)
 		{
-			product = (row * column);	
+			product = (row * column);
 			if (column == 0)
 			{
 				_putchar('0' + product);
@@ -32,4 +32,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
-}	
+}
